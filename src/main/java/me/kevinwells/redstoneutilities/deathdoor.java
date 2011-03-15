a5abaa403f21bd1c9902c188467cb197c3a0712d
Kevin Wells <wellsie1116@gmail.com>
2011-03-15 19:49:59 -0400

    implement deathdoor
    Signed-off-by: Kevin Wells <wellsie1116@gmail.com>

--- src/main/java/me/kevinwells/redstoneutilities/deathdoor.java
+++ src/main/java/me/kevinwells/redstoneutilities/deathdoor.java
@@ -5,8 +5,12 @@
 
 package me.kevinwells.redstoneutilities;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.bukkit.command.CommandSender;
 import org.tal.redstonechips.circuit.Circuit;
+
 
 /**
  *
@@ -14,15 +18,71 @@ import org.tal.redstonechips.circuit.Circuit;
  */
 public class deathdoor extends Circuit {
 
+	private int code[];
+	private int index = 0;
+	private boolean codeCorrect = true;
+	private boolean failed = false;
+	
+	private void sendDelayedOutput(final int index, final boolean state, long millis) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				sendOutput(index, state);
+			}
+		}, millis);
+	}
+	
     @Override
     public void inputChange(int index, boolean state) {
-        // This is where you handle changes in input state.
-        // The index is the input's pin number and state is true when the input turns high and false when it turns low.
+    	//inputs
+    	// pressure
+    	// btn1
+    	// btn2
+    	// btn3
 
-        // sets the lever position of the matching output pin according to the new state.
-        sendOutput(index, state);
-
-        if (hasDebuggers()) debug("Here's a debug message. Setting output " + index + " to " + (state?"on":"off"));
+    	//outputs
+    	// doors
+    	// failed
+    	// floor
+    	
+    	if (index == 0) {
+    		if (state) {
+    			if (hasDebuggers()) debug("Manually opening door");
+    			sendOutput(0, true);
+    		} else {
+    			if (hasDebuggers()) debug("Closing door in 1 second");
+    			sendDelayedOutput(0, false, 1000);
+    		}
+    	} else {
+    		if (state) {
+    			if (code[index] != index)
+    				codeCorrect = false;
+    			index++;
+    			if (index == code.length) {
+    				if (codeCorrect) {
+    					if (failed) {
+    						failed = false;
+    						sendOutput(1, false);
+    					}
+    					sendOutput(0, true);
+    					if (hasDebuggers()) debug("Code correct: opening door");
+    					
+    				} else if (!failed) {
+    					sendOutput(1, true);
+    					failed = true;
+    					if (hasDebuggers()) debug("Code incorrect: try one more time");
+    				} else {
+    					sendOutput(2, true);
+    					sendDelayedOutput(2, false, 500);
+    					failed = false;
+    					if (hasDebuggers()) debug("Code incorrect: goodbye");
+    				}
+    				index = 0;
+    				codeCorrect = true;
+    			}
+    		}
+    	}
     }
 
     @Override
@@ -30,7 +90,22 @@ public class deathdoor extends Circuit {
         // This code executes when you right-click on the circuit's sign and again each
         // time the the server is restarted or the RedstoneChips plugin is enabled.
 
-        info(sender, "You have activated LibraryTemplate test circuit.");
+        info(sender, "You have activated deathdoor test circuit.");
+
+        if (strings.length > 0) {
+	        String password = strings[0];
+	        code = new int[password.length()];
+	        
+	        for (int i = 0; i < password.length(); i++) {
+	        	if (!Character.isDigit(password.charAt(i))) {
+	        		error(sender, "Invalid password format");
+	        		return false;
+	        	}
+	        	code[i] = Integer.parseInt(String.valueOf(password.charAt(i)));
+	        }
+        } else {
+        	code = new int[] {2, 1, 3};
+        }
 
         return true;
     }
